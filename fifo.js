bfd0da877dd2b79722562cefbdef072fadacfb1e
Piotr Cichowlas <cichowlasp@Piotrs-MacBook-Pro.local>
2021-01-16 01:07:24 +0100

    implemented FIFO and started implementing LRU

---
+++ fifo.js
@@ -0,0 +1,37 @@
+const { performance } = require('perf_hooks');
+
+const FIFO = (size, pages) => {
+	const timeStart = performance.now();
+	let pageFaults = 0;
+	let pageHits = 0;
+	let memory = [];
+	let indexOfTheOldest = 0;
+	for (const el of pages) {
+		if (memory.length === size) {
+			if (memory.includes(el)) {
+				pageHits++;
+			} else {
+				memory[indexOfTheOldest] = el;
+				pageFaults++;
+			}
+			indexOfTheOldest =
+				indexOfTheOldest === size - 1 ? 0 : indexOfTheOldest++;
+		} else {
+			if (memory.includes(el)) {
+				pageHits++;
+			} else {
+				pageFaults++;
+				memory.push(el);
+			}
+		}
+	}
+	return {
+		name: 'FIFO',
+		memory,
+		pageFaults,
+		pageHits,
+		time: performance.now() - timeStart,
+	};
+};
+
+module.exports = FIFO;
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const input = require('prompt-sync')();
+const FIFO = require('./fifo');
 
 // Functions:
 const getPages = (number) => {
@@ -11,8 +12,43 @@ const getPages = (number) => {
 	return tab;
 };
 
+const LRU = (size, pages) => {
+	const timeStart = performance.now();
+	let pageFaults = 0;
+	let pageHits = 0;
+	let memory = [];
+	let lastRecentlyUsedIndex = 0;
+	for (const [el, index] of pages.entries()) {
+		if (memory.length === size) {
+			if (memory.includes(el)) {
+				pageHits++;
+			} else {
+				memory[lastRecentlyUsedIndex] = el;
+				pageFaults++;
+			}
+			lastRecentlyUsedIndex = (index) => {};
+		} else {
+			if (memory.includes(el)) {
+				pageHits++;
+			} else {
+				pageFaults++;
+				memory.push(el);
+			}
+		}
+	}
+	return {
+		memory,
+		pageFaults,
+		pageHits,
+		time: performance.now() - timeStart,
+	};
+};
+
 // Main:
 console.log('Page Replecament Simulation');
 const pagesNumber = input('How many pages do you want to generate? : ');
 const pages = getPages(pagesNumber);
+const size = 3;
+console.log('Your pages:');
 console.log(pages);
+console.log('FIFO: ', FIFO(size, pages));
