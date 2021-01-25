a0ca4ddec85d8c317ba9734ddd2b82d2f7fd0d89
Piotr Cichowlas <cichowlasp@Piotrs-MacBook-Pro.local>
2021-01-26 00:54:31 +0100

    had to change concept due to school requirements

--- index.js
+++ index.js
@@ -1,29 +1,55 @@
 const input = require('prompt-sync')();
-const FIFO = require('./fifo');
-const LRU = require('./lru');
-const LFU = require('./lfu');
-const MFU = require('./mfu');
+const { readFileSync, writeFileSync } = require('fs');
+const FIFO = require('./page-replacement/fifo');
+const LRU = require('./page-replacement/lru');
 
 // Functions:
-const getPages = (number) => {
+const generatePages = (number) => {
 	const tab = [];
 	const min = 0;
 	const max = 7;
 	for (let i = 0; i < number; i++) {
-		tab.push(Math.floor(Math.random() * (max - min + 1)) + min);
+		writeFileSync(
+			'page-examples.txt',
+			`${Math.floor(Math.random() * (max - min + 1)) + min}\n`,
+			{
+				flag: 'a',
+			}
+		);
 	}
 	return tab;
 };
 
 // Main:
-console.log('Page Replecament Simulation');
-const pagesNumber = input('How many pages do you want to generate? : ');
-const pages = getPages(pagesNumber);
-// const pages = [7, 0, 1, 2, 0, 3, 0, 4, 2, 3, 0, 3, 2, 3];
-const size = input('Type size : ');
+
+console.log('Project SO');
+const numberOfPages = input('Type number of pages to generate : ');
+generatePages(numberOfPages);
+const pages = readFileSync('page-examples.txt', 'utf-8').trim().split('\n');
+const size = 3;
 console.log('Your pages:');
 console.log(pages);
-console.log('FIFO: ', FIFO(size, pages));
-console.log('LRU: ', LRU(size, pages));
-console.log('LFU: ', LFU(size, pages));
-console.log('MFU: ', MFU(size, pages));
+const FIFOresults = FIFO(size, pages);
+console.log('FIFO: ', FIFOresults);
+const LRUresults = LRU(size, pages);
+console.log('LRU: ', LRUresults);
+console.log('Your results are written to results.txt file');
+writeFileSync(
+	'results.txt',
+	`FIFO:{
+		name: ${FIFOresults.name},
+		memory: ${FIFOresults.memory},
+		pageFaults: ${FIFOresults.pageFaults},
+		pageHits: ${FIFOresults.pageHits},
+		time: ${FIFOresults.time}
+	}\nLRU:{
+		name: ${LRUresults.name},
+		memory: ${LRUresults.memory},
+		pageFaults: ${LRUresults.pageFaults},
+		pageHits: ${LRUresults.pageHits},
+		time: ${LRUresults.time}
+	}`,
+	{
+		flag: 'w',
+	}
+);
